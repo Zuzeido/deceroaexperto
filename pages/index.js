e7497a18478dc4b9a7ec5ef9061c985e362e71bd
Zuzeido <79529059+Zuzeido@users.noreply.github.com>
2022-09-24 17:28:00 +0200

    metiendo mas datos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import {PostCard, Categories, PostWidget} from '../components'
-
+import { FeaturedPosts } from '../sections'
 import {getPosts} from '../services'
 
 
@@ -11,6 +11,7 @@ export default function Home({ posts }) {
         <title>De Cero a Experto</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
         {posts.map((post) => (<PostCard post={post.node} key={post.title}/>))}
