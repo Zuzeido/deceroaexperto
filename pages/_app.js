3370103f8a7fdbb4021541b5ff2d772b69007bdb
Zuzeido <79529059+Zuzeido@users.noreply.github.com>
2022-09-26 12:46:13 +0200

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import '../styles/globals.scss'
 import React, {useEffect, useState} from 'react'
 import {Layout} from '../components'
 import 'tailwindcss/tailwind.css'
+import ReactGA from 'react-ga'
+const TRACKING_ID = "UA-242789382-1"
+ReactGA.initialize(TRACKING_ID)
 
 function MyApp({ Component, pageProps }) {
   return (
